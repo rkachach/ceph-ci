2bdc209ded9904e147d16ab9ec3917bbf67d9a5a
Pritha Srivastava <prsrivas@redhat.com>
2021-10-18 11:23:17 +0530

    rgw: role metadata handler invokes RGWRole interfaces. Removing references to RGW services and RGW Ctl.
    Signed-off-by: Pritha Srivastava <prsrivas@redhat.com>

--- src/rgw/rgw_sal_rados.cc
+++ src/rgw/rgw_sal_rados.cc
@@ -34,6 +34,8 @@
 #include "rgw_rest_conn.h"
 #include "rgw_service.h"
 #include "services/svc_sys_obj.h"
+#include "services/svc_meta.h"
+#include "services/svc_meta_be_sobj.h"
 #include "services/svc_zone.h"
 #include "services/svc_tier_rados.h"
 #include "services/svc_quota.h"
@@ -3061,7 +3063,6 @@ int RadosRole::delete_obj(const DoutPrefixProvider *dpp, optional_yield y)
   }
   return ret;
 }
-
 
 } // namespace rgw::sal
 
