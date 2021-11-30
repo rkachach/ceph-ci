7e2214d49a09eda1869515c70a56298e72d6155a
Pritha Srivastava <prsrivas@redhat.com>
2021-11-30 13:48:01 +0530

    rgw: adding roles to non master ops list.
    Signed-off-by: Pritha Srivastava <prsrivas@redhat.com>

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -5707,7 +5707,11 @@ int main(int argc, const char **argv)
                                         OPT::METADATA_RM, OPT::RESHARD_CANCEL,
                                         OPT::RESHARD_ADD, OPT::MFA_CREATE,
                                         OPT::MFA_REMOVE, OPT::MFA_RESYNC,
-                                        OPT::CAPS_ADD, OPT::CAPS_RM};
+                                        OPT::CAPS_ADD, OPT::CAPS_RM,
+                                        OPT::ROLE_CREATE, OPT::ROLE_DELETE,
+                                        OPT::ROLE_GET, OPT::ROLE_LIST,
+                                        OPT::ROLE_POLICY_PUT, OPT::ROLE_POLICY_LIST,
+                                        OPT::ROLE_POLICY_GET, OPT::ROLE_POLICY_DELETE};
 
   bool print_warning_message = (non_master_ops_list.find(opt_cmd) != non_master_ops_list.end() &&
                                 non_master_cmd);
